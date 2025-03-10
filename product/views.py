1b337b20950c845d8b2ed9ee8ac8d30ee9ac2f64
Minte <163767827+minte-atnafu@users.noreply.github.com>
2025-03-10 21:56:04 +0300

    simple product list with postgresql and django

---
+++ product/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Product
+# Create your views here.
+
+def product_list(request):
+    products=Product.objects.all()
+    context={'products':products}
+    return render(request, 'product/product_list.html',context)
